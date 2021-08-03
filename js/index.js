34e44854897bc18c0e8156ef0e335024974ea041
Yulian-Stol <kokomoko12345koko@gmail.com>
2021-08-03 13:13:35 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -26,3 +26,25 @@ var output = document.getElementById("demo");
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+  function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+
+  /* When the user clicks on the button,
+  toggle between hiding and showing the dropdown content */
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
