81ec315a8f104917d780b0ef733c2909746a9d1f
Yulian-Stol <kokomoko12345koko@gmail.com>
2021-08-03 07:22:30 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@
 
   var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+// output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
