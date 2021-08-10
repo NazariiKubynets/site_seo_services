d3a1445dd00b7db2a77ff5b156209366b1bbc389
Yulian-Stol <kokomoko12345koko@gmail.com>
2021-08-10 14:24:39 +0000

    responsive mobile

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
   function openCity(evt, cityName) {
     var i, x, tablinks;
     x = document.getElementsByClassName("city");
@@ -78,4 +79,12 @@ for (let e of document.querySelectorAll('input[type="range"].slider-progress')) 
   e.style.setProperty('--max', e.max == '' ? '100' : e.max);
   e.addEventListener('input', () => e.style.setProperty('--value', e.value));
 }
+
+
+
+
+
+
+
+
 
